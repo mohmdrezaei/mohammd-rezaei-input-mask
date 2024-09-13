fc9dd9aa7b03a32f6089c13877d57fc7e61382d9
mohmdrezaei <mhwmdrz9@gmail.com>
2024-09-13 23:30:08 +0330

    display: flex;   justify-content: center;   align-items: center;q

--- src/CitySuggesstion.jsx
+++ src/CitySuggesstion.jsx
@@ -1,21 +1,28 @@
 import React, { useEffect, useState } from "react";
 import styles from "./CitySuggestion.module.css";
 import cities from "./cities.json";
+
+const BASE_URL = "https://api.opencagedata.com/geocode/v1";
+const API_KEY = "ef0a7cf762804bb38d6486f1cc231a5a";
 function CitySuggesstion() {
   const [inputValue, setInputValue] = useState("");
   const [suggestion, setSuggestion] = useState("");
   const [enterPressed, setEnterPressed] = useState(false);
+  const [coordinates, setCoordinates] = useState(null);
 
   useEffect(() => {
-    if (inputValue && inputValue === inputValue.charAt(0).toUpperCase() + inputValue.slice(1)) {
+    if (
+      inputValue &&
+      inputValue === inputValue.charAt(0).toUpperCase() + inputValue.slice(1)
+    ) {
       const match = cities.find((city) => city.startsWith(inputValue));
       setSuggestion(match ? match : "");
     } else {
       setSuggestion("");
     }
 
-    if (enterPressed) {
-      setSuggestion("");
+    if (enterPressed && suggestion) {
+      fetchCoordinates(suggestion);
       setEnterPressed(false);
     }
   }, [inputValue]);
@@ -31,14 +38,31 @@ function CitySuggesstion() {
     } else if (e.key === "Backspace") {
       setInputValue("");
       setSuggestion("");
+      setCoordinates(null);
+    }
+  };
+
+  const fetchCoordinates = async (city) => {
+    try {
+      const res = await fetch(`${BASE_URL}/json?q=${city}&key=${API_KEY}`);
+      const json = await res.json();
+      if (json.results.length > 0) {
+        const { lat, lng } = json.results[0].geometry;
+        setCoordinates({ lat, lng });
+        
+      } else {
+        setCoordinates(null);
+      }
+      
+    } catch (error) {
+      console.error("Error fetching coordinates:", error);
+      setCoordinates(null);
     }
   };
 
   return (
     <div className={styles.app}>
-   
       <div className={styles.container}>
-      
         <input
           type="text"
           value={inputValue}
@@ -53,7 +77,13 @@ function CitySuggesstion() {
             {suggestion.slice(inputValue.length)}
           </div>
         )}
+        
       </div>
+      {coordinates && (
+          <div className={styles.coordinates}>
+            Latitude: {coordinates.lat}, Longitude: {coordinates.lng}
+          </div>
+        )}
     </div>
   );
 }
